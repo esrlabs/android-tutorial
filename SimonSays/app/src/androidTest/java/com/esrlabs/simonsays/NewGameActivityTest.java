9738ba71cb5f232a1fc964d2a9a2dbd2a33940c0
Sebastian Benz <sebastian.benz@gmail.com>
2014-10-16 15:10:10 +0200

    finish step 3

---
+++ SimonSays/app/src/androidTest/java/com/esrlabs/simonsays/NewGameActivityTest.java
@@ -0,0 +1,14 @@
+package com.esrlabs.simonsays;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class NewGameActivityTest {
+
+    @Test
+    public void testName() throws Exception {
+        new NewGameActivity();
+        fail();
+    }
+}
